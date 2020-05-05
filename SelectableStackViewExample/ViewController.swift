44c750c4700df1887a63cf31994e3b6dc91a5e16
VladimirBrejcha <vladimirbrejcha@icloud.com>
2020-05-05 15:00:18 +0300

    Initial Commit

---
+++ SelectableStackViewExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SelectableStackViewExample
+//
+//  Created by Владимир Королев on 05.05.2020.
+//  Copyright © 2020 VladimirBrejcha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
